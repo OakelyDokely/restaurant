e143aed93aaee341667c003657f24656e3b1cb0c
OakelyDokely <60668649+OakelyDokely@users.noreply.github.com>
2024-03-01 20:59:23 +0000

    First attempt at models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Ingredient Name')
+    quantity = models.DecimalField(decimal_places=1, verbose_name="Ingredient Quantity")
+    price = models.DecimalField(decimal_places=2, verbose_name="Ingredient Price")
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Menu Item Name')
+    price = models.DecimalField(decimal_places=2, verbose_name='Menu Item Price')
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, verbose_name='Related Menu Item')
+    ingredient = models.ForeignKey(Ingredient, verbose_name='Ingredient')
+    quantity = models.DecimalField(decimal_places=1, verbose_name="Required Quantity")
+
+class Purchase(models.Model):
+    customer_name = models.CharField(max_length=100, verbose_name='Customer Name')
+    menu_item = models.ForeignKey(MenuItem, verbose_name='Purchased Menu Item')
+    purchase_date = models.DateTimeField(verbose_name='Purchase Date')
