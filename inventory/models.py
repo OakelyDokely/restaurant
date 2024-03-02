53462a26f9c982e75f194e13ff87b0ec2debd663
OakelyDokely <60668649+OakelyDokely@users.noreply.github.com>
2024-03-02 11:29:04 +0000

    Add base and homepage, related URLS. Correct models. Able to access home

--- inventory/models.py
+++ inventory/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, verbose_name='Ingredient Name')
-    quantity = models.DecimalField(decimal_places=1, verbose_name="Ingredient Quantity")
-    price = models.DecimalField(decimal_places=2, verbose_name="Ingredient Price")
+    quantity = models.DecimalField(max_digits=9, decimal_places=1, verbose_name="Ingredient Quantity")
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Ingredient Price")
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100, verbose_name='Menu Item Name')
-    price = models.DecimalField(decimal_places=2, verbose_name='Menu Item Price')
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Menu Item Price')
 
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, verbose_name='Related Menu Item')
-    ingredient = models.ForeignKey(Ingredient, verbose_name='Ingredient')
-    quantity = models.DecimalField(decimal_places=1, verbose_name="Required Quantity")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, verbose_name='Ingredient')
+    quantity = models.DecimalField(max_digits=6, decimal_places=1, verbose_name="Required Quantity")
 
 class Purchase(models.Model):
     customer_name = models.CharField(max_length=100, verbose_name='Customer Name')
-    menu_item = models.ForeignKey(MenuItem, verbose_name='Purchased Menu Item')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, verbose_name='Purchased Menu Item')
     purchase_date = models.DateTimeField(verbose_name='Purchase Date')
